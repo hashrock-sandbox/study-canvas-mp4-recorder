76e812f6c19eaedc239d42bb057054889f2fdfbb
hashrock <hashrock@users.noreply.github.com>
2021-04-27 21:16:13 +0900

    add painter

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ ctx.fillStyle = "white"
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 ctx.fillStyle = "black"
+let old = null
+ctx.lineWidth = 10
+canvas.addEventListener("pointerdown", (e) =>{
+    old = {
+        x: e.offsetX,
+        y: e.offsetY
+    }
+    ctx.moveTo(e.offsetX, e.offsetY)
+})
 canvas.addEventListener("pointermove", (e) =>{
-    ctx.fillRect(e.offsetX, e.offsetY, 10, 10)
+    if(old){
+        ctx.lineTo(e.offsetX, e.offsetY)
+        ctx.stroke()
+    }
+})
+canvas.addEventListener("pointerup", (e) =>{
+    old = null
 })
 
 CanvasRecorder(canvas)
